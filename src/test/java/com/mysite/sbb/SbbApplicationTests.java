c4847b4403705d1b15258f1fc9b6a75e5351a821
wjwj910 <wjwj910@gmail.com>
2024-12-11 09:47:02 +0900

    2-5, findBySubject

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -15,11 +15,7 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
-		Optional<Question> oq = questionRepository.findById(1);
-
-		if(oq.isPresent()) {
-			Question q = oq.get();
-			assertEquals("sbb가 무엇인가요?", q.getSubject());
-		}
+		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1, q.getId());
 	}
 }
